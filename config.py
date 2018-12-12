d295e52180debe0be0564dbe1b45f2c5af6c498d
Alexandre Reinsch <alexandre.reinsch@coding-academy.fr>
2018-12-12 17:20:57 +0100

    scrpit_working

---
+++ config.py
@@ -0,0 +1,14 @@
+keys = {
+        "product_url": "https://www.supremenewyork.com/shop/hats/usr9uodc8/mg4sbt5q7" ,
+        "name": "Reinsch Alexandre",
+        "email": "alexandre.reinsch@email.fr",
+        "phone_number": 1213141516,
+        "street_address": "1 rue des orangers",
+        "zip_code": 59000,
+        "city": "Lille",
+        "card_cvv": 111,
+        "card_number": 1452365545789532,
+        "card_exp_yr": 2020,
+        "card_exp_mo": 10
+}
+
